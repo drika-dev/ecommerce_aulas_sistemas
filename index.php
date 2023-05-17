90eb26e7b6ec3fcab4f472c2f1c58a1827469d2c
Adriane <adriane.desenvolvimento@gmail.com>
2023-05-16 21:50:31 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+    <title>
+        Minha Biblioteca
+    </title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <div id="pagina" class="pagina">
+        <div id="banner" class="banner">
+            <img src="imagem/banner.jpeg" id="bannerimg" class="bannerimg">
+        </div>
+        <div id="menu" class="menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Produtos</a>
+                </li>
+                <li>
+                    <a href="#">Cadastro</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Sobre/Contato</a>
+                </li>
+            </ul>
+        </div>    
+    <h1>Nosso Produtos</h1>
+    <table>
+        <tr>
+            <td>
+                <img src="imagem/dota2.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/leftfordead.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/phasmophobia.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/leftfordead.jpg" id="produtos" class="produtos">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label> Dota 2 </label>
+            </td>
+            <td> 
+                <label> Left For Dead 2 </label>
+            </td>
+            <td> 
+                <label>Phasmophobia </label>
+            </td>
+            <td> 
+                <label>Phasmophobia </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="imagem/dota2.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/leftfordead.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/phasmophobia.jpg" id="produtos" class="produtos">
+            </td>
+            <td>
+                <img src="imagem/leftfordead.jpg" id="produtos" class="produtos">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label> Dota 2 </label>
+            </td>
+            <td> 
+                <label> Left For Dead 2 </label>
+            </td>
+            <td> 
+                <label>Phasmophobia </label>
+            </td>
+            <td> 
+                <label>Phasmophobia </label>
+            </td>
+        </tr>
+</table>
+        Rodapé
+</div>
+</body>
+</-html>
