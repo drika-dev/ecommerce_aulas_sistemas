52d2e2544d045a34af4b0a22d75026fd2d3e8a27
Adriane <adriane.desenvolvimento@gmail.com>
2023-06-06 20:43:53 -0300

    Parte 2 06/06/2023

--- if-else.php
+++ if-else.php
@@ -1,21 +1,36 @@
 <?php
+include('components/menu.php');
+
 $numero1 = 10;
 $numero2 = 30;
 
 //IF
-if($numero1 == 10){
-    echo 'Número 1 é igual a 10.';
+echo '<br/> Condicional IF';
+if ($numero1 == 10) {
+    echo '<br/> Número 1 é igual a 10.';
 }
-if($numero2 == 20){
+if ($numero2 == 20) {
     echo '<br/> Numero 2 é igual a 20.';
 }
 // IF AND ELSE
-
+echo '<br/> Condicional IF and ELSE';
 $eduardo = 17;
 
-if($eduardo >= 18){
+if ($eduardo >= 18) {
     echo "<br/> Maior de idade.";
-}else{
+} else {
     echo "<br/> Menor de idade.";
 }
+echo '<br/> Repetição FOR';
+for ($i = 0; $i <= 10; $i++) {
+    echo '<div id="minhadiv" class="minhadiv">Valor do indice' . $i . '<br/> </div> ';
+}
+
+echo '<br/> Repetição While<br/> ';
+$indice = 0;
+while($indice != 10){
+    echo '<br/> Indice: '. $indice;
+    $indice++;
+}
+include('components/rodape.php');
 ?>
